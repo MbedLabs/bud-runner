745521b6abbf857e645d5122b0b611fbc48023c9
El Omari Amine <54433252+elomariamin@users.noreply.github.com>
2026-04-19 03:53:01 +0200

    chore: remove mate prefix support, use bud only

--- bud_runner/bloom_client.py
+++ bud_runner/bloom_client.py
@@ -447,7 +447,7 @@ class BloomClient:
         if not tc:
             test_methods = [
                 m for m in dir(test_class)
-                if m.startswith("mate_") and callable(getattr(test_class, m, None))
+                if m.startswith("bud_") and callable(getattr(test_class, m, None))
             ]
 
             full_desc = description
