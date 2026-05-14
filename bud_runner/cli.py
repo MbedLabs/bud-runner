80d06c739063296b1c401ebbe98c6c2e3dfb3535
El Omari Amine <54433252+elomariamin@users.noreply.github.com>
2026-05-14 02:19:04 +0200

    chore: bump package version to 0.4.4

--- bud_runner/cli.py
+++ bud_runner/cli.py
@@ -579,7 +579,7 @@ def version():
         typer.echo(f"bud_runner version {v}")
     except Exception:
         # 3. Hardcoded fallback
-        typer.echo("bud_runner version 0.4.3")
+        typer.echo("bud_runner version 0.4.4")
 
 
 def main():
